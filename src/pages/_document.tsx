f413037a81181ae87987be6fdd8dbeaf85d294b7
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2021-03-16 19:00:44 -0300

    Added meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,11 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
-          <title>Início | Move.it</title>
-          
+          <title>Move.it</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="description" content="Não fique parado em frente ao computador por longas horas, se movimente. Essa é a proposta do Move.it" />
+          <meta name="keywords" content="health, move, saúde, contador, haruo"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
         </Head>
